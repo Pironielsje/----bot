84ae749f259aa95554e7dffcda2ad8292ad99a2e
AlgoGames <81696606+Pironielsje@users.noreply.github.com>
2021-06-09 11:46:06 +0200

    help command
    halp

---
+++ commands/help.js
@@ -0,0 +1,47 @@
+const { MessageEmbed } = require('discord.js');
+const db = require('quick.db')
+
+module.exports.run = async(client, message, args) => {
+    var commandsList = []
+    const prefix = db.get(`prefix_${message.guild.id}`)
+
+    client.commands.forEach((command) => {
+        var constructor = {
+            name: command.help.name,
+            description: command.help.description,
+            category: command.help.category,
+            aliases: command.help.aliases
+        }
+
+        commandsList.push(constructor)
+    });
+
+    let response = new MessageEmbed()
+        .setTitle("**Help - (☞ﾟヮﾟ)☞      ☜(ﾟヮﾟ☜)")
+        .setDescription("All commands for (☞ﾟヮﾟ)☞      ☜(ﾟヮﾟ☜).")
+        .setColor("RANDOM")
+        .setFooter(message.author.tag, message.author.displayAvatarURL())
+
+    let info = response.addField("Info", "Information commands. \n")
+
+    for (let i = 0; i < commandsList.length; i++) {
+        let command = commandsList[i]
+
+        if (command["category"] == "Info") {
+            info += `**${prefix}${command["name"]} - Description: ${command["description"]} Aliases: ${command["aliases"]}\ns`
+        }
+
+    }
+
+    response.addField(info)
+
+    message.channel.send(response)
+
+}
+
+module.exports.help = {
+    name: "help",
+    description: "Send the help message.",
+    category: "Info",
+    aliases: ["yeet", "h"]
+}
--- index.js
+++ index.js
@@ -1,8 +1,50 @@
 const discord = require('discord.js')
 const client = new discord.Client()
+const db = require('quick.db')
+const fs = require('fs')
+
+client.commands = new discord.Collection()
+client.aliases = new discord.Collection()
+
+fs.readdir('./commands/', (err, file) => {
+    if (err) return console.log(err)
+
+    let jsFiles = file.filter(f => f.split('.').pop() === "js")
+
+    if (jsFiles < 0) {
+        console.log('No files found.')
+        return
+    }
+
+    jsFiles.forEach((f, i) => {
+        var getFile = require(`./commands/${f}`)
+
+        console.log(`Succesfully loaded ${f}.`)
+
+        client.commands.set(getFile.help.name, getFile)
+
+        getFile.help.aliases.forEach(alias => {
+            client.aliases.set(alias, getFile.help.name)
+        })
+    })
+})
 
 client.on('ready', () => {
     console.log('ready')
+})
+
+client.on('message', async message => {
+    if (!db.get(`prefix_${message.guild.id}`)) {
+        db.set(`prefix_${message.guild.id}`)
+    }
+
+    let prefix = db.get(`prefix_${message.guild.id}`)
+
+    let args = message.content.slice(prefix.length).trim().split(" ")
+    let command = args.shift().toLowerCase()
+    let commands = client.commands.get(command) || client.commands.get(client.aliases.get(command))
+
+    if (commands) client.commands.run(client, message, args)
 })
 
 client.login(process.env.token)
